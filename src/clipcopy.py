60a5382c08dd39b7b708b5986a0fe67fa9b8abee
hoang-long2012 <hoanglongdeptrai392@gmail.com>
2026-07-31 00:25:17 +0700

    Update version number.

--- src/clipcopy.py
+++ src/clipcopy.py
@@ -83,7 +83,7 @@ dir | %(prog)s   Places a copy of the current directory listing into the Windows
 	ArgsList = normalizeWindowsArgs(sys.argv[1:])
 	Parser = argparse.ArgumentParser(prog="Clipcopy", description="Copy your standard input or text to clipboard.", epilog=Examples, formatter_class=argparse.RawDescriptionHelpFormatter, allow_abbrev=False)
 	Parser.add_argument("text", type=str, help="Text to copy to the clipboard.")
-	Parser.add_argument("-v", "--version", action="version", version="%(prog)s version 1.1")
+	Parser.add_argument("-v", "--version", action="version", version="%(prog)s version 1.2")
 	Parser.add_argument("-s", "--show", action="store_true", help="Show copy content to the standard output.")
 	return Parser.parse_args(ArgsList)
 def main():
